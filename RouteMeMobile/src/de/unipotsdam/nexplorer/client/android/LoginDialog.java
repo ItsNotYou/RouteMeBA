8bb71a50db36627bcd60581b587292518cf96a08
Hendrik Geßner <hgessner@uni-potsdam.de>
2013-03-19 00:39:32 +0100

    Login-Text korrigiert.

--- RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/LoginDialog.java
+++ RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/LoginDialog.java
@@ -2,10 +2,16 @@ package de.unipotsdam.nexplorer.client.android;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class LoginDialog extends Dialog {
+
+	private LoginCallback loginListener;
 
 	public LoginDialog(Context context) {
 		super(context);
@@ -13,18 +19,39 @@ public class LoginDialog extends Dialog {
 		setContentView(R.layout.dialog_login);
 		setTitle("Anmeldung");
 		setCancelable(false);
-	}
 
-	public void setOnLoginListener(final LoginCallback listener) {
+		EditText loginName = (EditText) findViewById(R.id.login_name);
+		loginName.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == R.id.login_attempt || actionId == EditorInfo.IME_NULL) {
+					attemptLogin();
+					return true;
+				}
+				return false;
+			}
+		});
+
 		findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				EditText text = (EditText) findViewById(R.id.login_name);
-				String name = text.getText().toString();
-				listener.onLogin(name);
+				attemptLogin();
 			}
 		});
+	}
+
+	private void attemptLogin() {
+		EditText text = (EditText) findViewById(R.id.login_name);
+		String name = text.getText().toString();
+		if (loginListener != null) {
+			loginListener.onLogin(name);
+		}
+	}
+
+	public void setOnLoginListener(final LoginCallback listener) {
+		this.loginListener = listener;
 	}
 
 	public interface LoginCallback {
