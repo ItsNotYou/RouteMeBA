c4baaee9e0e499254ee56b8f8b4a6cbaebe3d658
Hendrik Geßner <hgessner@uni-potsdam.de>
2013-03-20 02:12:34 +0100

    GPS-Dummy implementiert.

--- RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/js/Geolocation.java
+++ RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/js/Geolocation.java
@@ -3,15 +3,18 @@ package de.unipotsdam.nexplorer.client.android.js;
 import static android.location.LocationManager.GPS_PROVIDER;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 
 public class Geolocation {
+
+	private static boolean IS_DEBUG = true;
 
 	private Context context;
 	private Activity host;
@@ -21,22 +24,27 @@ public class Geolocation {
 		this.host = host;
 	}
 
-	public void clearWatch(ActiveGeolocator positionWatch) {
+	public void clearWatch(Geolocator positionWatch) {
 		if (positionWatch != null) {
 			positionWatch.clear();
 		}
 	}
 
-	public ActiveGeolocator watchPosition(FunctionsMobile functionsMobile, NavigatorOptions navigatorOptions) {
-		return new ActiveGeolocator(context, functionsMobile, host);
+	public Geolocator watchPosition(FunctionsMobile functionsMobile, NavigatorOptions navigatorOptions) {
+		if (IS_DEBUG) {
+			return new DummyGeolocator(functionsMobile);
+		} else {
+			return new ActiveGeolocator(context, functionsMobile, host);
+		}
 	}
 
-	public class ActiveGeolocator implements LocationListener {
+	public class ActiveGeolocator extends Geolocator {
 
 		private LocationManager manager;
 		private FunctionsMobile callback;
 
 		public ActiveGeolocator(Context context, FunctionsMobile callback, Activity host) {
+			super(callback);
 			this.callback = callback;
 			this.manager = registerManager(context);
 			host.runOnUiThread(new Runnable() {
@@ -48,6 +56,7 @@ public class Geolocation {
 			});
 		}
 
+		@Override
 		public void clear() {
 			manager.removeUpdates(this);
 		}
@@ -60,29 +69,47 @@ public class Geolocation {
 
 			return manager;
 		}
+	}
 
-		@Override
-		public void onLocationChanged(Location location) {
-			de.unipotsdam.nexplorer.client.android.support.Location result = new de.unipotsdam.nexplorer.client.android.support.Location();
-			result.setLatitude(location.getLatitude());
-			result.setLongitude(location.getLongitude());
-			result.setAccuracy(location.getAccuracy());
-			result.setHeading(location.getBearing());
-			result.setSpeed(location.getSpeed());
+	public class DummyGeolocator extends Geolocator implements Runnable {
 
-			callback.positionReceived(result);
+		private LocationGenerator locations;
+		private Random random;
+		private TimerTaskWrapper task;
+
+		public DummyGeolocator(FunctionsMobile callback) {
+			super(callback);
+			this.locations = new LocationGenerator(52.394317333573106, 13.129833250863612, 52.39410782938052, 13.131571322305263);
+			this.random = new Random();
+			this.task = new TimerTaskWrapper(this);
+
+			locations.generateStartLocation(0);
+			new Timer().schedule(task, 5000, 1000);
 		}
 
 		@Override
-		public void onProviderDisabled(String arg0) {
+		public void run() {
+			Location loc = locations.generateNextLocation(random.nextDouble() * 2, random.nextInt(91) - 45);
+			super.onLocationChanged(loc);
 		}
 
 		@Override
-		public void onProviderEnabled(String provider) {
+		public void clear() {
+			task.cancel();
 		}
 
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
+		private class TimerTaskWrapper extends TimerTask {
+
+			private Runnable runnable;
+
+			public TimerTaskWrapper(Runnable runnable) {
+				this.runnable = runnable;
+			}
+
+			@Override
+			public void run() {
+				runnable.run();
+			}
 		}
 	}
 }
