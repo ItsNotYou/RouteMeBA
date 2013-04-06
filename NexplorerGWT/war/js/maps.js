11af194ed7c9b0107501b67c3e86d250850fcc82
Hendrik Geßner <hgessner@uni-potsdam.de>
2013-04-06 03:51:01 +0200

    Nachbar-Linien um Pfeile erweitert.

--- NexplorerGWT/war/js/maps.js
+++ NexplorerGWT/war/js/maps.js
@@ -7,9 +7,16 @@ function NeighbourLine(map) {
 		var from = new google.maps.LatLng(nodes.from.latitude, nodes.from.longitude);
 		var to = new google.maps.LatLng(nodes.to.latitude, nodes.to.longitude);
 		
-		var line = new google.maps.Polyline(
-		{
+		var lineSymbol = {
+			path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+		};
+		
+		var line = new google.maps.Polyline({
 			path: [from, to],
+			icons: [{
+				icon: lineSymbol,
+				offset: '100%'
+			}],
 			map: map,
 			clickable: false
 		});
