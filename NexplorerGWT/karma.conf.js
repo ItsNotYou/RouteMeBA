3daac118526354a323ecaba45dae7fb663a3e77c
Hendrik Geßner <hgessner@uni-potsdam.de>
2013-04-17 16:49:33 +0200

    Testbrowser korrigiert.

--- NexplorerGWT/karma.conf.js
+++ NexplorerGWT/karma.conf.js
@@ -58,7 +58,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['IE'];
+browsers = ['Chrome'];
 
 
 // If browser does not capture in given timeout [ms], kill it
