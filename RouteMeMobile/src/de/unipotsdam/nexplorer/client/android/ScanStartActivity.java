f98943c4371362333864daf93606135c52d894ad
Hendrik Geßner <hgessner@uni-potsdam.de>
2013-07-08 20:57:21 +0200

    Added code scanning.

--- RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/ScanStartActivity.java
+++ RouteMeMobile/src/de/unipotsdam/nexplorer/client/android/ScanStartActivity.java
@@ -5,9 +5,13 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanStartActivity extends Activity {
 
@@ -23,20 +27,58 @@ public class ScanStartActivity extends Activity {
 		new OnlineCheckTask().execute();
 	}
 
+	/**
+	 * Reacts to the given connection status.
+	 * 
+	 * @param isConnected
+	 *            Calls {@link #forwardToMap()} if {@code true}
+	 */
 	private void setIsConnected(boolean isConnected) {
 		if (isConnected) {
 			forwardToMap();
 		}
 	}
 
+	/**
+	 * Starts a {@link MapActivity}.
+	 */
 	private void forwardToMap() {
 		Intent intent = new Intent(this, MapActivity.class);
 		startActivity(intent);
 	}
 
+	/**
+	 * Start a scan. As a result, {@link #onActivityResult(int, int, Intent)} is called.
+	 * 
+	 * @param view
+	 */
 	public void onScanClicked(View view) {
+		IntentIntegrator intent = new IntentIntegrator(this);
+		intent.initiateScan();
 	}
 
+	/**
+	 * Starts a {@link ScannedActivity} if a scan result was given.
+	 * 
+	 * @see IntentIntegrator#initiateScan()
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		if (scanResult != null) {
+			String result = scanResult.getContents();
+
+			Intent intent = new Intent(this, ScannedActivity.class);
+			intent.setData(Uri.parse(result));
+			startActivity(intent);
+		}
+	}
+
+	/**
+	 * Calls {@link #forwardToMap()}.
+	 * 
+	 * @param view
+	 */
 	public void onForwardClicked(View view) {
 		forwardToMap();
 	}
