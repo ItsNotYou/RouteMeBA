a5b0cb7b6a53b7819e9cbd044cd9b64199114036
Hendrik Geßner <HENGE01@gmail.com>
2013-10-08 23:03:36 +0200

    Corrected test setup.

--- NexplorerGWT/test/de/unipotsdam/nexplorer/server/persistence/hibernate/DatabaseLocatorTest.java
+++ NexplorerGWT/test/de/unipotsdam/nexplorer/server/persistence/hibernate/DatabaseLocatorTest.java
@@ -2,6 +2,7 @@ package de.unipotsdam.nexplorer.server.persistence.hibernate;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -13,6 +14,7 @@ import de.unipotsdam.nexplorer.server.Admin;
 import de.unipotsdam.nexplorer.server.data.Unit;
 import de.unipotsdam.nexplorer.server.persistence.DatabaseImpl;
 import de.unipotsdam.nexplorer.server.persistence.Player;
+import de.unipotsdam.nexplorer.server.persistence.hibernate.dto.Players;
 import de.unipotsdam.nexplorer.server.persistence.hibernate.dto.Settings;
 
 public class DatabaseLocatorTest {
@@ -26,6 +28,49 @@ public class DatabaseLocatorTest {
 		try {
 			DatabaseImpl dbAccess = unit.resolve(DatabaseImpl.class);
 			dbAccess.persist(settings);
+
+			Players players = new Players();
+			players.setBattery(100d);
+			players.setHasSignalRangeBooster(0l);
+			players.setHasSignalStrengthBooster(null);
+			Date now = new Date();
+			players.setLastPositionUpdate(now.getTime());
+			players.setLatitude(52.3935);
+			players.setLongitude(13.130513);
+			players.setName("testplayer");
+			players.setRole((byte) 2);
+			players.setScore(0l);
+			players.setSequenceNumber(1l);
+			players.setBaseNodeRange(settings.getBaseNodeRange());
+			dbAccess.persist(players);
+
+			Players two = new Players();
+			two.setBattery(100.);
+			two.setHasSignalRangeBooster(0l);
+			two.setHasSignalStrengthBooster(null);
+			two.setLastPositionUpdate(new Date().getTime());
+			two.setLatitude(52.39345);
+			two.setLongitude(13.130513);
+			two.setName("player two");
+			two.setRole((byte) 2);
+			two.setScore(0l);
+			two.setSequenceNumber(1l);
+			two.setBaseNodeRange(settings.getBaseNodeRange());
+			dbAccess.persist(two);
+
+			Players third = new Players();
+			third.setBattery(100.);
+			third.setHasSignalRangeBooster(0l);
+			third.setHasSignalStrengthBooster(0l);
+			third.setLastPositionUpdate(new Date().getTime());
+			third.setLatitude(52.3934);
+			third.setLongitude(13.130513);
+			third.setName("player three");
+			third.setRole((byte) 2);
+			third.setScore(0l);
+			third.setSequenceNumber(15l);
+			third.setBaseNodeRange(settings.getBaseNodeRange());
+			dbAccess.persist(third);
 		} finally {
 			unit.close();
 		}
