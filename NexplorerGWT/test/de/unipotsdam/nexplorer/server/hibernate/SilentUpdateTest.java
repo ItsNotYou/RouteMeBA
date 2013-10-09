eb313e3021405d965587aef390e69f2481de88a7
Hendrik Geßner <HENGE01@gmail.com>
2013-10-09 15:59:34 +0200

    Added a new silent insert test.

--- NexplorerGWT/test/de/unipotsdam/nexplorer/server/hibernate/SilentUpdateTest.java
+++ NexplorerGWT/test/de/unipotsdam/nexplorer/server/hibernate/SilentUpdateTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.assertEquals;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.unipotsdam.nexplorer.server.persistence.hibernate.HibernateSessions;
+import de.unipotsdam.nexplorer.server.persistence.hibernate.dto.AodvDataPackets;
 import de.unipotsdam.nexplorer.server.persistence.hibernate.dto.Players;
 
 public class SilentUpdateTest {
@@ -20,6 +22,19 @@ public class SilentUpdateTest {
 		Players p = new Players();
 		p.setName("Hallo");
 		session.save(p);
+
+		session.flush();
+		tx.commit();
+		session.close();
+	}
+
+	@After
+	public void after() {
+		Session session = HibernateSessions.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+
+		Players p = (Players) session.createCriteria(Players.class).uniqueResult();
+		session.delete(p);
 
 		session.flush();
 		tx.commit();
@@ -45,5 +60,31 @@ public class SilentUpdateTest {
 		session.close();
 
 		assertEquals("Welt", result);
+	}
+
+	@Test
+	public void shouldInsertMessageSilently() {
+		Session session = HibernateSessions.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+
+		Players p = (Players) session.createCriteria(Players.class).uniqueResult();
+		AodvDataPackets packet = new AodvDataPackets();
+		packet.setHopsDone((short) 3);
+		packet.setPlayersByOwnerId(p);
+		p.getAodvDataPacketsesForOwnerId().add(packet);
+
+		session.flush();
+		tx.commit();
+		session.close();
+
+		// Assert
+		session = HibernateSessions.getSessionFactory().openSession();
+		packet = (AodvDataPackets) session.createCriteria(AodvDataPackets.class).uniqueResult();
+		short result = packet.getHopsDone();
+		Long playerId = packet.getPlayersByOwnerId().getId();
+		session.close();
+
+		assertEquals(3, result);
+		assertEquals(p.getId(), playerId);
 	}
 }
